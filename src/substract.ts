bf6a711ed6c1fd827499ee37a196b08fdcf0634d
Sergio Hernán Romano <sergio.h.romano@gmail.com>
2023-02-16 15:14:23 -0300

    api-extractor basic setup

---
+++ src/substract.ts
@@ -0,0 +1,11 @@
+/**
+ * Calculate the difference between two numbers
+ * 
+ * @param a - first number
+ * @param b - second number
+ * 
+ * @beta
+ */
+export function substract(a: number, b: number): number {
+    return a - b;
+}
